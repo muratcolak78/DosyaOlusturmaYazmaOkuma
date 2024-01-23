efc5eddc7f7ee4f4c9fc757e754689c9b5c88963
murat colak <colakmurat-78@hotmail.com>
2024-01-23 07:32:27 +0100

    Projektdateien hinzufügen.

---
+++ DosyaOlusturmaYazmaOkuma/Dosya.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DosyaOlusturmaYazmaOkuma
+{
+    internal class Dosya
+    {
+
+        private string dosyaYolu = "C:\\Users\\murat\\dosyaOlusturYazOku\\";
+        private string dosyaAdi;
+        public Dosya(string dosyaAdi) { 
+            this.dosyaAdi = dosyaAdi;
+        this.dosyaYolu += dosyaAdi;
+            DosyaOlustur();
+        }
+
+
+        public void DosyaOlustur()
+        {
+            if (!File.Exists(dosyaYolu))
+            {
+                Console.WriteLine(dosyaAdi+" başarılı bir şekilde oluşturuldu");
+                File.Create(dosyaYolu).Close();
+            }
+        }
+
+
+
+        public void DosyayaYaz(string metin)
+        {
+            
+            // Dosyayı yazma modunda aç
+            using (StreamWriter sw = new StreamWriter(dosyaYolu, true))
+            {
+                // Metni dosyaya yaz
+                sw.WriteLine(metin);
+            }
+
+            Console.WriteLine("Başarıyla yazıldı.");
+        }
+
+        public string DosyadanOku()
+        {
+            // Dosya var mı kontrol et
+            if (!File.Exists(dosyaYolu))
+            {
+                Console.WriteLine("Dosya bulunamadı.");
+                return null;
+            }
+
+            // Dosyayı okuma modunda aç
+            using (StreamReader sr = new StreamReader(dosyaYolu))
+            {
+                // Dosyadan metni oku
+                string okunanMetin = sr.ReadToEnd();
+                return okunanMetin;
+            }
+        }
+    }
+}
---
+++ DosyaOlusturmaYazmaOkuma/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DosyaOlusturmaYazmaOkuma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dosya dosya = new Dosya("murat2.bat");
+            //dosya.DosyayaYaz("bus dosyaya bir şeyler yazıldı");
+            //Console.WriteLine(dosya.DosyadanOku());
+
+        }
+
+
+    }
+}
+
